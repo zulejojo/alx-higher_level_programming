d0def32190882f4f04a31d20eb447ea97b0076cb
zulejojo <zulejojo@gmail.com>
2024-08-23 13:45:08 -0700

    to uppercase

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,5 +5,5 @@ def print_last_digit(number):
         print(-(last_digit), end='')
     else:
         last_digit = number % 10
-        print(last_digit, end= '')
+        print(last_digit, end='')
     return abs(last_digit)
