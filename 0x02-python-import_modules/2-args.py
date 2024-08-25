13508590e727c3f71275430524ab9e4f9b9235a6
zulejojo <zulejojo@gmail.com>
2024-08-25 07:40:33 -0700

    dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,8 @@ if __name__ == "__main__":
     if x < 1:
         print("{} arguments.".format(x))
     elif x == 1:
-        print("{} argument:".format(x)
+        print("{} argument:".format(x))
     else:
         print("{} arguments:".format(x))
-
     for i in range(x):
         print("{}: {:s}".format(i + 1, argv[i + 1]))
