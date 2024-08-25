26890a9c6cd3f48282d049bd122c75c1712ca4cc
zulejojo <zulejojo@gmail.com>
2024-08-25 07:18:21 -0700

    dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     from sys import argv
     x = len(argv) -1
     if x < 1:
-        print("{} argumnets.".format(x))
+        print("{} arguments.".format(x))
     elif x == 1:
-        print("{} argumnets.".format(x))
+        print("{} arguments.".format(x))
     else:
-        print("{} argumnets.".format(x))
+        print("{} arguments.".format(x))
 
     for i in range(x):
-        print("{}:(:s)".format(i +1, argv[i +1]))
+        print("{}:{:s}".format(i + 1, argv[i + 1]))
