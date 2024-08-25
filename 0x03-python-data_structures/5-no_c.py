e780fe95a421e41012faee5cbaa2a39028cf200e
zulejojo <zulejojo@gmail.com>
2024-08-25 15:30:29 -0700

    Can you c me

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
         new_string = my_string.translate({ord("c"):None})
         second_string = new_string.translate({ord("C"):None})
         return second_string
-    
+    return my_string
