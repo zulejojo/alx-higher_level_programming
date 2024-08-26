a70792b8a262f0342be0ecd5ca7e9f4fa4f7c4d4
zulejojo <zulejojo@gmail.com>
2024-08-26 03:25:12 -0700

    search and replace

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new-dico = {}
+    new_dico = {}
     if a_dictionary:
         for key, value in a_dictionary.items():
             new_dico.update({key: value * 2})
