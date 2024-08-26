1430475fec1a30dffd5c6c16cd3bc0ca9243a2f2
zulejojo <zulejojo@gmail.com>
2024-08-26 02:48:06 -0700

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,5 +4,5 @@ def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for index, items in enumerate(new_list):
         if items == search:
-            new_list [index] = replace
+            new_list[index] = replace
     return new_list
