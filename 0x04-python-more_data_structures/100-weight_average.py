b29958f39d3a3909961dca9c25239c3107161232
zulejojo <zulejojo@gmail.com>
2024-08-26 04:09:39 -0700

    search and replace

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return (0)
+    sum_total = 0
+    div = 0
+    for item in my_list:
+        sum_total += item[0] * item[1]
+        div +=item[-1]
+    return sum_total/div
