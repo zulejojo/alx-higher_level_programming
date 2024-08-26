ec6e69d855602c232fd71aac80d198d6d3fa1a29
zulejojo <zulejojo@gmail.com>
2024-08-26 03:08:39 -0700

    search and replace

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    keys = []
+    if a_dictionary:
+        for a, b in a_dictionary.items():
+            keys.append(a)
+
+            keys.sort()
+            for items in keys:
+                print("{}: {}".format(items, a_dictionary[items]))
