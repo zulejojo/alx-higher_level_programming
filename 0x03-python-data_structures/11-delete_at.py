e68ea26f7f09eef33273eebb8792d5727aae0097
zulejojo <zulejojo@gmail.com>
2024-08-26 00:53:41 -0700

    multiple_returns

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     length = len(my_list)
-    if idx > length -1:
+    if idx > length - 1:
         return my_list
     elif idx < 0:
         return my_list
