bacf29ee8ddb7ad48983f4e200fe72ba0527bc9d
zulejojo <zulejojo@gmail.com>
2024-08-26 03:23:51 -0700

    search and replace

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new-dico = {}
+    if a_dictionary:
+        for key, value in a_dictionary.items():
+            new_dico.update({key: value * 2})
+    return new_dico
