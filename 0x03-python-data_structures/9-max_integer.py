07475dc419af1ce8f8743a43c98f4312d8490de4
zulejojo <zulejojo@gmail.com>
2024-08-25 17:15:19 -0700

    more returns

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     new_list = []
     if my_list:
-        my_list.sort(reverse=True)
+        my_list.sort(reverse = True)
         return(my_list[0])
     return(None)
                             
