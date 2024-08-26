10309a9db99c49909cd900e06e635d52cf2df73f
zulejojo <zulejojo@gmail.com>
2024-08-25 17:26:03 -0700

    more returns

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
 def max_integer(my_list=[]):
         if not my_list:
             return None
+
         max_value = my_list[0]  # Start with the first element as the maximum
         for num in my_list[1:]:
             if num > max_value:  # Update max_value if a larger number is found
                 max_value = num
+
         return max_value
