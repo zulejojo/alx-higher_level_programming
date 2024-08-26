42a16cf46a5728eaf71fbb335ebadb9247033164
zulejojo <zulejojo@gmail.com>
2024-08-26 03:33:55 -0700

    search and replace

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def best_score(a_dictionary):
     if a_dictionary:
-        my_list = list(a-dictionary.keys())
+        my_list = list(a_dictionary.keys())
         score = 0
         leader = ""
         for i in my_list:
