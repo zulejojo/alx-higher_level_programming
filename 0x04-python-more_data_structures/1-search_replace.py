3e12ad7eb2a58030df555b063500556de7a248e0
zulejojo <zulejojo@gmail.com>
2024-08-26 02:47:01 -0700

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for index, items in enumerate(new_list):
+        if items == search:
+            new_list [index] = replace
+    return new_list
