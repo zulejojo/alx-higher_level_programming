ce39a3ffc493f3f6dc16cdd7963f3eff5c3bcb91
zulejojo <zulejojo@gmail.com>
2024-08-28 19:40:15 -0700

    sql introduction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,8 @@
--- Step 1: Convert the database character set and collation to utf8mb4
-ALTER DATABASE `hbtn_0c_0` CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+-- Step 1: Convert the database charset and collation
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Step 2: Convert the table character set and collation to utf8mb4
-ALTER TABLE `hbtn_0c_0`.`first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Step 2: Convert the table charset and collation
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Step 3: Convert the specific column (name) to utf8mb4
-ALTER TABLE `hbtn_0c_0`.`first_table` MODIFY `name` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Step 3: Convert the field `name` charset and collation
+ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
